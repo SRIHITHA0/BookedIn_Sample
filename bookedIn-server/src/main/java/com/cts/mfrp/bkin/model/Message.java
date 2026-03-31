439a6563831a20e0792822e1709fd9afa7710d4e
Kohad <2480004@cognizant.com>
2026-03-31 11:17:38 +0530

    database entities

--- bookedIn-server/src/main/java/com/cts/mfrp/bkin/model/Message.java
+++ bookedIn-server/src/main/java/com/cts/mfrp/bkin/model/Message.java
@@ -1,4 +1,43 @@
 package com.cts.mfrp.bkin.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "messages")
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // The user who sends the message
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    // The user who receives the message
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+
+    // Using @Column(columnDefinition = "TEXT") if messages can be very long
+    @Column(nullable = false, length = 2000)
+    private String content;
+
+    // Defaulting to false for new messages
+    @Column(nullable = false)
+    private Boolean isRead = false;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime sentAt;
 }
