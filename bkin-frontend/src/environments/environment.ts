aa1b63be2c5c403c3559ebcd631b7db9d9a00ecf
SRIHITHA0 <138369032+SRIHITHA0@users.noreply.github.com>
2026-05-10 21:44:46 +0530

    Update environment.ts

--- bkin-frontend/src/environments/environment.ts
+++ bkin-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080' // Or 9090, whatever your IntelliJ uses
+  apiUrl: 'http://localhost:8090' // Or 9090, whatever your IntelliJ uses
 };
